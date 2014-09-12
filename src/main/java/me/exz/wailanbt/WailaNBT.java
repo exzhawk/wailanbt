6f7b007c3822b103bd55eab38f80a59c0aa1698c
Epix <git@exz.me>
2014-09-13 06:26:59 +0800

    init class

---
+++ src/main/java/me/exz/wailanbt/WailaNBT.java
@@ -0,0 +1,22 @@
+package me.exz.wailanbt;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "WailaNBT", name = "Waila NBT", version = "1.7.10-1.0")
+public class WailaNBT {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
