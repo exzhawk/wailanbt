bbadae09c9cbf2df2e3f7a47543aac27d338207d
Epix <git@exz.me>
2014-09-15 17:30:43 +0800

    helper&dep

--- src/main/java/me/exz/wailanbt/WailaNBT.java
+++ src/main/java/me/exz/wailanbt/WailaNBT.java
@@ -4,21 +4,25 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import me.exz.wailanbt.reference.Reference;
 
-@Mod(modid = "WailaNBT", name = "Waila NBT", version = "1.7.10-1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class WailaNBT {
-    @Mod.Instance("WailaNBT")
+    @Mod.Instance(Reference.MOD_ID)
     public static WailaNBT instance;
+
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event){
+    public void preInit(FMLPreInitializationEvent event) {
 
     }
+
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event){
+    public void init(FMLInitializationEvent event) {
 
     }
+
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event){
+    public void postInit(FMLPostInitializationEvent event) {
 
     }
 }
---
+++ src/main/java/me/exz/wailanbt/reference/Reference.java
@@ -0,0 +1,7 @@
+package me.exz.wailanbt.reference;
+
+public class Reference {
+    public static final String MOD_ID = "WailaNBT";
+    public static final String MOD_NAME = "Waila NBT";
+    public static final String VERSION = "1.7.10-1.0";
+}
