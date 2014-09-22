0fb1a0946a5fb2cf8bda5b461038922d6ed16ece
Epix <git@exz.me>
2014-09-23 01:51:37 +0800

    bump version to 1.1

--- src/main/java/me/exz/wailanbt/reference/Reference.java
+++ src/main/java/me/exz/wailanbt/reference/Reference.java
@@ -3,5 +3,5 @@ package me.exz.wailanbt.reference;
 public class Reference {
     public static final String MOD_ID = "WailaNBT";
     public static final String MOD_NAME = "Waila NBT";
-    public static final String VERSION = "1.7.10-1.0";
+    public static final String VERSION = "1.7.10-1.1";
 }
