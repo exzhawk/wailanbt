f2a5d42970fe022c49fc7174f6b2971e6f61dba7
Epix <git@exz.me>
2014-09-24 18:46:59 +0800

    fix folder in config caused crash

--- src/main/java/me/exz/wailanbt/configuration/config.java
+++ src/main/java/me/exz/wailanbt/configuration/config.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import me.exz.wailanbt.reference.Reference;
 import me.exz.wailanbt.util.LogHelper;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -44,20 +45,22 @@ public class config {
         File[] configFiles = configDir.listFiles();
         if (!(configFiles == null)) {
             for (File configFile : configFiles) {
-                try {
-                    InputStream inputStream = new FileInputStream(configFile);
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                    JsonParser parser = new JsonParser();
-                    JsonElement jsonElementCurrent = parser.parse(inputStreamReader);
-                    if (jsonElementCurrent.isJsonObject()) {
-                        JsonObject jsonObjectCurrent = jsonElementCurrent.getAsJsonObject();
-                        mergeJson(jsonObjectCurrent);
-                    } else {
-                        LogHelper.error("Parse " + configFile.toString() + " failed");
+                if (configFile.isFile()) {
+                    try {
+                        InputStream inputStream = new FileInputStream(configFile);
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        JsonParser parser = new JsonParser();
+                        JsonElement jsonElementCurrent = parser.parse(inputStreamReader);
+                        if (jsonElementCurrent.isJsonObject()) {
+                            JsonObject jsonObjectCurrent = jsonElementCurrent.getAsJsonObject();
+                            mergeJson(jsonObjectCurrent);
+                        } else {
+                            LogHelper.error("Parse " + configFile.toString() + " failed");
+                        }
+                    } catch (Exception e) {
+                        //e.printStackTrace();
+                        LogHelper.error("Error parsing file '" + configFile.getName() + "'. Possible error: " + e.getCause().getMessage());
                     }
-                } catch (Exception e) {
-                    //e.printStackTrace();
-                    LogHelper.error("Error parsing file '"+configFile.getName()+"'. Possible error: "+ e.getCause().getMessage());
                 }
             }
         }
@@ -72,14 +75,14 @@ public class config {
                 if (configJson.has(holdItem.getKey())) {
                     JsonObject configJsonHoldItem = configJson.get(holdItem.getKey()).getAsJsonObject();
                     Set<Map.Entry<String, JsonElement>> teA = holdItem.getValue().getAsJsonObject().entrySet();
-                    for(Map.Entry<String, JsonElement> te:teA){
-if (!configJsonHoldItem.has(te.getKey())){
-    configJsonHoldItem.add(te.getKey(),te.getValue());
-}
+                    for (Map.Entry<String, JsonElement> te : teA) {
+                        if (!configJsonHoldItem.has(te.getKey())) {
+                            configJsonHoldItem.add(te.getKey(), te.getValue());
+                        }
                     }
                     configJson.remove(holdItem.getKey());
-                    configJson.add(holdItem.getKey(),configJsonHoldItem);
-                }else{
+                    configJson.add(holdItem.getKey(), configJsonHoldItem);
+                } else {
                     configJson.add(holdItem.getKey(), holdItem.getValue());
                 }
             }
